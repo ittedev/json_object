44f032734e6bdce7f767ba2d58cce49a4999dfa8
Nakamura <57395168+itte1@users.noreply.github.com>
2022-07-28 16:01:24 +0000

    Add undefined property

--- json_object.ts
+++ json_object.ts
@@ -4,5 +4,5 @@ import { JSONValue } from './json_value.ts'
  * JSONObject is an object with JSONValue as a property value.
  */
 export type JSONObject = {
-  [key: string]: JSONValue
+  [key: string]: JSONValue | undefined
 }
